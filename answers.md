e1a60a6f87fe787ab9d8cb64de2834c2838841a5
vizualtxt <88392631+vizualtxt@users.noreply.github.com>
2023-10-26 04:59:38 +0000

    final answers i think

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Reciation 5
 ## Answers
 
-**Name:**_________________________
+**Name:** Ali Sulehria
 
 
 Place all written answers from `recitation-05.md` here for easier grading.
@@ -13,8 +13,38 @@ Place all written answers from `recitation-05.md` here for easier grading.
 
 
 - **1b.**
+The table below lists the averages over ten runs of three sorting algorithms for various values of n. We can see that random pivot quicksort takes a similar, though slightly shorter, time to complete as compared to the fixed pivot. The selection short performs favorably for small lists, but grows much too quickly. 
 
+|     n |   qsort-fixed-pivot |   qsort-random-pivot |    ssort |
+|-------|---------------------|----------------------|----------|
+|    10 |               0.020 |                0.026 |    0.008 |
+|   100 |               0.203 |                0.328 |    0.211 |
+|   200 |               0.764 |                0.721 |    1.376 |
+|   300 |               3.845 |                2.518 |    2.160 |
+|   400 |               1.112 |                5.418 |    6.109 |
+|   500 |               6.890 |                9.872 |   20.209 |
+|  1000 |              12.995 |                5.925 |   37.593 |
+|  2000 |              12.079 |               19.113 |  138.135 |
+|  5000 |              53.636 |               45.270 |  923.664 |
+| 10000 |              99.795 |              111.526 | 3734.287 |
 
+The random and fixed pivot, both being quicksort, are of the order O(n) = log^2(n), which holds for these recurrences. 
 
+For the case where the input is in order, the quicksort algorithms performed similarly. This is likely because the quicksort algorithm's speed isn't very dependent on whether the list is sorted or not - the only way to change it would be by changing the pivot selection function and the inputs.
 
 - **1c.**
+
+|     n |   qsort-fixed-pivot |   qsort-random-pivot |   timsort |
+|-------|---------------------|----------------------|-----------|
+|    10 |               0.000 |                0.000 |     0.000 |
+|   100 |               1.000 |                1.001 |     0.000 |
+|   200 |               0.994 |                2.652 |     0.000 |
+|   300 |               2.339 |                2.032 |     0.000 |
+|   400 |               4.000 |                3.000 |     0.000 |
+|   500 |               3.955 |                5.175 |     0.000 |
+|  1000 |               8.958 |               11.113 |     0.281 |
+|  2000 |              11.959 |               16.287 |     1.005 |
+|  5000 |              42.298 |               46.584 |     0.809 |
+| 10000 |              87.956 |               92.281 |     3.060 |
+
+Tim blew my sorting algorithms out of the water and it's really not very fair. The algorithm is significantly faster at every value. wow!
--- main.py
+++ main.py
@@ -1,71 +1,102 @@
-import random, time
+import random
+import time
+
 import tabulate
 
 
 def qsort(a, pivot_fn):
-    ## TO DO
-    pass
-    
+
+  if len(a) == 0 or len(a) == 1:
+    return a
+  p = pivot_fn(a)
+  left = list(filter(lambda x: x<p, a))
+  right  = list(filter(lambda x: x>p, a))
+
+  (sL, sR) = (qsort(left, pivot_fn), qsort(right, pivot_fn))
+  
+  return sL + [p] + sR
+
+  pass
+
+def ssort(L):
+  for i in range(len(L)):
+      #print(L)
+      m = L.index(min(L[i:]))
+      L[i], L[m] = L[m], L[i]
+  return L
+
 def time_search(sort_fn, mylist):
-    """
-    Return the number of milliseconds to run this
-    sort function on this list.
+  """
+  Return the number of milliseconds to run this
+  sort function on this list.
 
-    Note 1: `sort_fn` parameter is a function.
-    Note 2: time.time() returns the current time in seconds. 
-    You'll have to multiple by 1000 to get milliseconds.
+  Note 1: `sort_fn` parameter is a function.
+  Note 2: time.time() returns the current time in seconds. 
+  You'll have to multiple by 1000 to get milliseconds.
 
-    Params:
-      sort_fn.....the search function
-      mylist......the list to search
-      key.........the search key 
+  Params:
+    sort_fn.....the search function
+    mylist......the list to search
+    key.........the search key 
 
-    Returns:
-      the number of milliseconds it takes to run this
-      search function on this input.
-    """
+  Returns:
+    the number of milliseconds it takes to run this
+    search function on this input.
+  """
+  timeArr = []
+  for i in range(10):
     start = time.time()
     sort_fn(mylist)
-    return (time.time() - start) * 1000
-    ###
+    runt = (time.time() - start) * 1000
+    timeArr.append(runt)
+  return (sum(timeArr)/len(timeArr))
 
-def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]):
-    """
-    Compare the running time of different sorting algorithms.
 
-    Returns:
-      A list of tuples of the form
-      (n, linear_search_time, binary_search_time)
-      indicating the number of milliseconds it takes
-      for each method to run on each value of n
-    """
-    ### TODO - sorting algorithms for comparison
-    qsort_fixed_pivot = # 
-    qsort_random_pivot = #
-    tim_sort = #
-    result = []
-    for size in sizes:
-        # create list in ascending order
-        mylist = list(range(size))
-        # shuffles list if needed
-        #random.shuffle(mylist)
-        result.append([
-            len(mylist),
-            time_search(qsort_fixed_pivot, mylist),
-            time_search(qsort_random_pivot, mylist),
-        ])
-    return result
-    ###
+  ###
+
+def compare_sort(sizes=[10, 100, 200, 300, 400, 500, 1000, 2000, 5000, 10000]):
+  """
+  Compare the running time of different sorting algorithms.
+
+  Returns:
+    A list of tuples of the form
+    (n, linear_search_time, binary_search_time)
+    indicating the number of milliseconds it takes
+    for each method to run on each value of n
+  """
+  ### TODO - sorting algorithms for comparison
+  def qsort_fixed_pivot(a):
+    return qsort(a, lambda x: x[0])
+  def qsort_random_pivot(a):
+    return qsort(a, random.choice)
+  tim_sort = sorted
+  result = []
+  for size in sizes:
+      # create list in ascending order
+      mylist = list(range(size))
+      # shuffles list if needed
+      random.shuffle(mylist)
+    
+      result.append([
+          len(mylist),
+          time_search(qsort_fixed_pivot, mylist),
+          time_search(qsort_random_pivot, mylist),
+          time_search(tim_sort, mylist),
+      ])
+  return result
+  ###
 
 def print_results(results):
-    """ change as needed for comparisons """
-    print(tabulate.tabulate(results,
-                            headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot'],
-                            floatfmt=".3f",
-                            tablefmt="github"))
+  """ change as needed for comparisons """
+  print(tabulate.tabulate(results,
+                          headers=['n', 'qsort-fixed-pivot', 
+                                   'qsort-random-pivot', 
+                                   'timsort'],
+                          floatfmt=".3f",
+                          tablefmt="github"))
 
 def test_print():
-    print_results(compare_sort())
+  print_results(compare_sort())
 
 random.seed()
 test_print()
